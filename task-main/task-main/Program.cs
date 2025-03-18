505d125124d56a594abd8ed5363c337500439c05
yourunb <bxyura@gmail.com>
2025-03-18 13:15:36 +0300

    feat: add collection variables

--- task-main/task-main/Program.cs
+++ task-main/task-main/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿//---------------------------------------------------Коллекции--------------------------------------------------
+using System.Collections;
+
+List<int> myGenericList = new List<int> { 0, 1, 2, 3, 4, 5 }; // объявление обобщенного списка
+ArrayList myList = new ArrayList { 0, 1, 2, 3, 4 }; // объявление списка с числами // или ArrayList myList = new ArrayList() { 0, 1, 2, 3,4 };
+
+
+
+
